a6c323ead42ca3c37f9ff7ce82b8ec98e22d6d5c
Siji <126935176+Cerminsiji@users.noreply.github.com>
2026-02-16 01:12:00 +0700

    Add SOURCES array with dataset URLs

---
+++ update.js
@@ -0,0 +1,6 @@
+const SOURCES = [
+"https://raw.githubusercontent.com/Cerminsiji/sukslan-id-dataset/main/news_sample.txt",
+"https://raw.githubusercontent.com/Cerminsiji/sukslan-id-dataset/main/subtitle_sample.txt",
+"https://raw.githubusercontent.com/Cerminsiji/sukslan-id-dataset/main/umkm_story.txt",
+"https://raw.githubusercontent.com/Cerminsiji/sukslan-id-dataset/main/wikipedia_sample.txt"
+];
